d08da93931fb4bb8e60cce8b2f62b5ca719af008
xshen2470 <143212968+xshen2470@users.noreply.github.com>
2025-01-06 13:53:06 -0500

    Update kernel_prediction.m
    Add comments

--- kernel_prediction.m
+++ kernel_prediction.m
@@ -1,6 +1,8 @@
 function [pred, estimate] = kernel_prediction(train_data, test_data, train_score, lambda,type, scale)
-% pred is the prediction of based on the test data
-% estimate is the estimate from the training data
+% This function runs the kernel ridge regression
+% pred is the prediction of the score based on the test data
+% estimate is the estimate from the training data (should match exactly the train_score)
+% lambda is the hyper-parameter of the model
 
 [train_within, test_train] = generate_kernel(train_data, test_data, type, scale);
 
